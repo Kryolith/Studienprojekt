c9b88759b4b6fb10b0656fca226fe66fe5a6971d
Kryolith <sebastian@gerntke.eu>
2014-11-04 13:37:15 +0100

    + Constructor + registerRule function + handle function
    alles erstmal basics damit man damit testen kann

--- Studienprojekt/src/studienprojekt/RuleManager.java
+++ Studienprojekt/src/studienprojekt/RuleManager.java
@@ -6,10 +6,33 @@
 
 package studienprojekt;
 
+import java.util.ArrayList;
+import java.util.List;
+import studienprojekt.osm.OSMCoordinate;
+import studienprojekt.osm.OSMMap;
+import studienprojekt.rules.Rule;
+
 /**
  *
  * @author Sebastian
  */
 public class RuleManager {
+    
+    private List<Rule> rules;
+    
+    public RuleManager() {
+        this.rules = new ArrayList();
+    }
+    
+    public void registerRule(Rule rule) {
+        this.rules.add(rule);
+    }
+    
+    public void handle(OSMMap map, OSMCoordinate coordinate, SpaceUsageRule sur) {
+        
+        for(Rule rule : rules) {
+            if(rule.handles(sur)) rule.execute(map, coordinate, sur);
+        }
+    }
     
 }
