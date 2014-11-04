62a1719dc23dc890b190a975244516af7b3e8975
Kryolith <sebastian@gerntke.eu>
2014-11-04 13:19:40 +0100

    Run-Routine hinzugefügt

--- Studienprojekt/src/studienprojekt/Mapper.java
+++ Studienprojekt/src/studienprojekt/Mapper.java
@@ -5,6 +5,14 @@
  */
 package studienprojekt;
 
+import java.io.IOException;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import studienprojekt.osm.OSMCoordinate;
+import studienprojekt.osm.OSMMap;
+import studienprojekt.osm.OSMParser;
+
 /**
  *
  * @author Sebastian
@@ -26,5 +34,40 @@ public class Mapper {
         // load latest.properties
         // else
         // generate default configuration
+    }
+    
+    public void run() {
+        // Initialisiere InfileHandler mit Dateipfad des Testdatensatz
+        InfileHandler ifh = new InfileHandler("Testdatensatz/Data.txt");
+        
+        // Initialisiere OutfileHandler mit Dateipfad des Ausgabeordners
+        //OutfileHandler ofh = new OutfileHandler("Ausgabe0001/");
+        
+        // Lade erste Zeile um die Anzahl der Datensätze zu ermitteln
+        int datacount = Integer.parseInt(ifh.getNextLineArray().get(0));
+        
+        // Rufe nächste Zeile vom Infilehandler ab
+        List<String> line = ifh.getNextLineArray();
+        
+    	/* Ab hier Martins Teil */
+        // OSMParser erzeugen
+        OSMParser osmParser = new OSMParser();
+        
+        // Lege OSMCoordinate aus den Daten der Textfile an
+    	OSMCoordinate surCoordinate = new OSMCoordinate(Double.parseDouble(line.get(1)), Double.parseDouble(line.get(2)));
+        
+        System.out.println(surCoordinate);
+        
+    	OSMMap areaToCheck = null;
+        
+        try {
+            areaToCheck = osmParser.getOSMMap(surCoordinate, 0.1);
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            System.out.println(ex);
+        }
+
+        System.out.println("TESTAUSGABE: auslesen des Inhalts des generierten OSMMap-Objekts:");
+        System.out.print(areaToCheck);
+        /* Martins Teil Ende */
     }
 }
