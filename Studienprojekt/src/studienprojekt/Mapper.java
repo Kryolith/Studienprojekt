41debf76177235cb88f8af8597707b2a7c818746
Kryolith <sebastian@gerntke.eu>
2014-11-06 14:01:27 +0100

    - Ordentlich kommentiert - Für Dateiausgabe erweitert

--- Studienprojekt/src/studienprojekt/Mapper.java
+++ Studienprojekt/src/studienprojekt/Mapper.java
@@ -35,41 +35,63 @@ public class Mapper {
     }
     
     public void run() {
-        // Initialisiere InfileHandler mit Dateipfad des Testdatensatz aaabbbb6
+        // Initialisiere InfileHandler mit Dateipfad des Testdatensatz
         InfileHandler ifh = new InfileHandler("Testdatensatz/Data.txt");
         
-        // Initialisiere OutfileHandler mit Dateipfad des Ausgabeordners
-        //OutfileHandler ofh = new OutfileHandler("Ausgabe0001/");
+        //Initialisiere OutfileHandler mit Dateipfad des Ausgabeordners
+        OutfileHandler ofh = new OutfileHandler("result/");
         
         // Lade erste Zeile um die Anzahl der Datensätze zu ermitteln
         int datacount = Integer.parseInt(ifh.getNextLineArray().get(0));
         
-        // Rufe nächste Zeile vom Infilehandler ab
-        List<String> line = ifh.getNextLineArray();
+        // Limitiere datacount für Testzwecke
+        datacount = datacount > 1 ? 1 : datacount;
         
-    	/* Ab hier Martins Teil */
-        // OSMParser erzeugen
+        // OSMParser-Objekt erzeugen, damit mit der Map-API kommuniziert werden kann
         OSMParser osmParser = new OSMParser();
         
-        // Lege OSMCoordinate aus den Daten der Textfile an
-    	OSMCoordinate surCoordinate = new OSMCoordinate(Double.parseDouble(line.get(2)), Double.parseDouble(line.get(1)));
-        
-        System.out.println(surCoordinate);
-        
-    	OSMMap areaToCheck = null;
-        
-        try {
-            areaToCheck = osmParser.getOSMMap(surCoordinate, 0.001);
-        } catch (ParserConfigurationException | SAXException | IOException ex) {
-            System.out.println(ex);
-        }
-
-        //System.out.println("TESTAUSGABE: auslesen des Inhalts des generierten OSMMap-Objekts:");
-        //System.out.print(areaToCheck);
-        
-        this.ruleManager.handle(areaToCheck, surCoordinate, SpaceUsageRule.parseSpaceUsageRule(line.get(3)));
-        
-        
-        /* Martins Teil Ende */
+        // Iteriere durch alle Zeilen der Eingabedatei und behandel die eingetragenen SURs
+        for(int i = 0; i < datacount; i++) {
+            
+            // Erstelle und initialisiere ein Result-Objekt für das Ergebnis dieser Zeile
+            Result result = new Result();
+            
+            // Lade nächste Zeile aus dem Infilehandler als String-Array
+            List<String> line = ifh.getNextLineArray();
+            
+            // Setze name im Ergebnisobjekt (Name Feld 0 der Zeile)
+            result.setName(line.get(0));
+            
+            // Erstelle ein OSMCoordinate-Objekt mit den Koordinaten aus der Zeile
+            OSMCoordinate surCoordinate = new OSMCoordinate(Double.parseDouble(line.get(2)), Double.parseDouble(line.get(1)));
+            
+            // Setze Koordinaten im Ergebnisobjekt
+            result.setOSMCoordinate(surCoordinate);
+            
+            // Parse die SpaceUsageRule aus Feld 3 der Zeile
+            SpaceUsageRule currentSur = SpaceUsageRule.parseSpaceUsageRule(line.get(3));
+            
+            // AUch zum Ergebnis-Objekt hinzufügen
+            result.setSpaceUsageRule(currentSur);
+            
+            // Erstelle OSMMap-Objekt, dass die Gegend um die Koordinaten beschreibt
+            OSMMap areaToCheck = null;
+                        
+            // Füllen der Map mit Daten
+            try {
+                areaToCheck = osmParser.getOSMMap(surCoordinate, 0.001);
+            } catch (ParserConfigurationException | SAXException | IOException ex) {
+                System.out.println(ex);
+            }
+            
+            // Falls erfolgreich weiter (ansonsten sollte eh ein Fehler ausgegeben worden sein, evtl ist die Bedingung hier überflüssig?!?)
+            if(areaToCheck != null) {
+                // Gib die aktuellen Daten an den RegelManager weiter und speicher die Rückgabe im result-Objekt
+                result.setOSMWays(this.ruleManager.handle(areaToCheck, surCoordinate, currentSur));
+                
+                // Schlussendlich wird das Ergebnis noch abgespeichert über den OutfileHandler
+                ofh.saveData(result);
+            }
+        }        
     }
 }
