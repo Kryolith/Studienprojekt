1ccf110d9eb023eaf345c74f4b7941998d7e05d3
Kryolith <sebastian@gerntke.eu>
2014-11-06 14:00:54 +0100

    - Ordentlich kommentiert - An neues Ausgabeformat und Result-Klasse angepasst

--- Studienprojekt/src/studienprojekt/OutfileHandler.java
+++ Studienprojekt/src/studienprojekt/OutfileHandler.java
@@ -1,35 +1,60 @@
 package studienprojekt;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Iterator;
+import java.util.List;
+import studienprojekt.osm.OSMNode;
 
 public class OutfileHandler {
     
-    private String path;
-    private File dir;
+    private final String path;
     
     public OutfileHandler(String path) {
         this.path = path;
-        this.dir = new File(this.path);
+        File dir = new File(this.path);
         
-        if(!this.dir.exists()) {
-            this.dir.mkdir();
-        }
+        if(!dir.exists())
+            dir.mkdir();
     }    
     
-    public void saveData(String data, String filename) {
-        File outfile = new File(this.path + File.separator + filename);
-        FileWriter outfileWriter;
+    public void saveData(Result result) {
         
-        try {
-            outfileWriter = new FileWriter(outfile);
-            outfileWriter.write(data);
-            outfileWriter.close();
-        } catch (IOException ex) {
-            Logger.getLogger(OutfileHandler.class.getName()).log(Level.SEVERE, null, ex);
+        // Erstelle ein TemplateObject für die Ausgabedatei
+        Template tpl = new Template();
+        
+        // Lade vorlage
+        tpl.load("templates/truth.kml.tpl");
+        
+        // Füge Name im Template ein
+        tpl.replace("name", result.getName());
+        
+        if(result.getOSMWays().size() > 1)
+            throw new UnsupportedOperationException("Kann derzeit nur einen Weg als Ergebnis speichern");
+        
+        if(result.getOSMWays().isEmpty())
+            throw new UnsupportedOperationException("Kann keine leeren Ergebnisse behandeln");
+
+        // Lade ersten Weg aus Result (muss später durch einen Loop o.ä. ersetzt werden)
+        List<OSMNode> currentWay = result.getOSMWays().get(0).getWayComponents();
+        
+        // Iteriere durch alle Nodes
+        for (Iterator<OSMNode> it = currentWay.iterator(); it.hasNext();) {
+            OSMNode node = it.next();
+            
+            // Baue Node-String
+            String coordinateString = 
+                    node.getNodeCoordinate().getLongitude() + "," + 
+                    node.getNodeCoordinate().getLatitude();
+            
+            // Falls es noch einen nächsten gibt füge einen Zeilenumbruch an
+            if(it.hasNext())
+                coordinateString += "\n"; 
+            
+            // Ersetze Coordinates-Loop mit der aktuellen Zeile
+            tpl.replace("coordinates", coordinateString);
         }
+        
+        // Speicher Template ab
+        tpl.save(path + "/" + result.getName() + "." + result.getId() + ".truth.kml");
     }  
 }
